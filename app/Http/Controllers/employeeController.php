455efc8ab40eb90e9071074dfce2c5cd9e0cffcd
Yekchey Flores <yekcheyf@gmail.com>
2026-05-24 23:28:36 +0800

    try mo ulit

--- app/Http/Controllers/employeeController.php
+++ app/Http/Controllers/employeeController.php
@@ -8,7 +8,7 @@ class employeeController extends Controller
 {
     public function index()
     {
-        $employees = \App\Models\employee::all();
+        $employees = employee::all();
         return view('employee.index', compact('employees'));
     }
 
@@ -28,14 +28,14 @@ class employeeController extends Controller
             'address' => 'required',
         ]);
 
-        \App\Models\employee::create($request->all());
+        employee::create($request->all());
 
         return redirect()->route('employee.index')->with('success', 'Employee created successfully.');
     }
 
     public function edit($id)
     {
-        $employees = \App\Models\employee::findOrFail($id);
+        $employees = employee::findOrFail($id);
         return view('employee.edit', compact('employees'));
     }
 
@@ -50,7 +50,7 @@ class employeeController extends Controller
             'address' => 'required',
         ]);
 
-        $employees = \App\Models\employee::findOrFail($id);
+        $employees = employee::findOrFail($id);
         $employees->update($request->all());
 
         return redirect()->route('employee.index')->with('success', 'Employee updated successfully.');
@@ -58,7 +58,7 @@ class employeeController extends Controller
 
     public function destroy($id)
     {
-        $employees = \App\Models\employee::findOrFail($id);
+        $employees = employee::findOrFail($id);
         $employees->delete();
 
         return redirect()->route('employee.index')->with('success', 'Employee deleted successfully.');
